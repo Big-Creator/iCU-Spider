b31474146598e8d1e0ceacc14aabe5e01fc8a567
joelYing <39625260+joelYing@users.noreply.github.com>
2018-09-01 21:08:14 +0800

    学校简介

--- xxjj.py
+++ xxjj.py
@@ -44,34 +44,6 @@ class Spider():
         test.insert(data)
         print 'Success!'
 
-    # def toMysql(self):
-    #     data = self.getData()
-    #     #建立python与MySQL之间的连接
-    #     mysql = pymysql.connect(host="localhost",user="root",passwd="12138",db="json",charset="utf8")
-    #     #新建游标
-    #     cur = mysql.cursor()
-    #     #原生sql语句，创建一个名为jsondata的表，并在其中定义字段
-    #     sqlc = """
-    #         create table if not exists jsondata(
-    #         typeid int(11) not null auto_increment primary key,
-    #         tagid varchar(20) not null,
-    #         title varchar(100) not null,
-    #         time varchar(20) not null,
-    #         content mediumtext not null)DEFAULT CHARSET=utf8;
-    #      """
-    #     cur.execute(sqlc)
-    #     mysql.commit()
-    #     print 'success'
-    #     sqla = """
-    #         insert into jsondata(typeid,tagid,title,time,content)values(%s,%s,%s,%s,%s);
-    #     """
-    #     #这里的data数据为列表，data[0]中存在的字典类型数据
-    #     cur.execute(sqla, (data[0]['typeid'], data[0]['tagid'], data[0]['title'], data[0]['time'], data[0]['content']))
-    #     mysql.commit()
-    #     cur.close()
-    #     mysql.close()
-    #     print 'success'
-
 if __name__ == '__main__':
     spider = Spider()
     spider.toMongoDB()
